bf367f60ee0cfe6c61b895421a6159492c1805e3
Armando del Río <d-rio@outlook.com>
2020-06-17 01:51:22 -0500

    Code cleaning

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-// pages
+import { Flex } from '@chakra-ui/core';
 import AuthPage from './pages/AuthPage';
 import MainPage from './pages/MainPage';
-
-//components
-import { Flex } from '@chakra-ui/core';
-
 import Footer from './components/Footer';
 import PrivateRoute from './components/PrivateRoute';
 import Navbar from './components/Navbar';
--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,25 +1,21 @@
 import React, { useState, useContext, createContext } from 'react';
 
 import handleAsync from '../services/handleAsync';
-
-//Axios config
 import axios from 'axios';
+
 const baseURL = 'https://voldemort.klustera.com';
 const service = axios.create({ baseURL });
 
-// Create auth context
 const authContext = createContext();
 export function ProvideAuth({ children }) {
   const auth = useProvideAuth();
   return <authContext.Provider value={auth}>{children}</authContext.Provider>;
 }
 
-// Hook for child components to get the auth object and re-render when it changes.
 export const useAuth = () => {
   return useContext(authContext);
 };
 
-// Provider hook that creates auth object and handles state
 function useProvideAuth() {
   const [token, setToken] = useState(null);
   const login = async credentials => {
